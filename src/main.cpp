1e1c48fe7eee6b8d79e0205df1a9ef714b0ae77a
Andrii Tsykhmeistruk <andretsyk1324@gmail.com>
2026-05-05 20:22:37 +0300

    Homework 1.6 Main Task

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,20 @@
 #include <Arduino.h>
 
-#define BUTTON_PIN 4
-
-volatile uint32_t edgeCounter = 0;
-
-void IRAM_ATTR buttonISR() {
-  edgeCounter++;
-}
+#define LDR_PIN 4
 
 void setup() {
   Serial.begin(115200);
-
-  pinMode(BUTTON_PIN, INPUT); // зовнішній pull-down
-  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonISR, RISING);
 }
 
 void loop() {
-  static bool measuring = false;
-  static uint32_t startCount = 0;
-  static uint32_t lastSeenCount = 0;
-  static uint32_t lastEdgeTime = 0;
+  uint16_t  raw = analogRead(LDR_PIN);
+  double voltage_calc = raw * 3.3 / 4095.0;
+  uint16_t  voltage_mv = analogReadMilliVolts(LDR_PIN);
+  double voltage_mv_v = voltage_mv / 1000.0;
+  double error = fabs(voltage_calc - voltage_mv_v) / voltage_mv_v * 100;
 
-  uint32_t count = edgeCounter;
+  Serial.printf("RAW:  %u | Calculated Voltage: %.3f V | analogReadMilliVolts: %.3f V | Error(%): %.2f %%\n",
+    raw, voltage_calc, voltage_mv_v, error);
 
-  if (!measuring && count != lastSeenCount) {
-    measuring = true;
-    startCount = lastSeenCount;
-    lastEdgeTime = millis();
-  }
-
-  if (measuring && count != lastSeenCount) {
-    lastSeenCount = count;
-    lastEdgeTime = millis();
-  }
-
-  if (measuring && digitalRead(BUTTON_PIN) == LOW && millis() - lastEdgeTime > 100) {
-    Serial.printf("One full press RISING3 edges: %lu\n", lastSeenCount - startCount);
-    measuring = false;
-  }
+  delay(100);
 }
